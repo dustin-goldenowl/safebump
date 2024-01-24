8f4ab9a1cd085502c652c48eb0d7b3abc0315ffb
lance-go <lance.dinh.goldenowl@gmail.com>
2024-01-24 13:55:35 +0700

    Push hard color in to color manager class

--- lib/src/theme/colors.dart
+++ lib/src/theme/colors.dart
@@ -10,6 +10,7 @@ class AppColors {
   static const divider = Color(0xFFEFEFEF);
   static const scaffoldBackgroundColor = Color(0xFFF8F6EE);
   static const background = Color.fromARGB(255, 249, 247, 242);
+  static const greyScaleLine = Color.fromARGB(255, 188, 194, 203);
   static const hintTextColor = Color(0xFF8C8A8A);
   static const red = Color.fromARGB(255, 248, 103, 103);
   static const green = Color.fromARGB(255, 61, 164, 61);
